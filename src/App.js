f00c1dcf5cd0bdcb1ec0749dbfd22b79296a1ff9
Jordybastien <rugumbirajordybastien@gmail.com>
2021-02-15 14:21:07 +0200

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Routing from './router';
+import { connect } from 'react-redux';
+import { decodeToken, tokenKey } from './services/auth';
+import { setAuthedUser } from './actions/authedUser';
+import { handleInitialData } from './actions/initialData';
+import ReactLoading from 'react-loading';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+    this.props.dispatch(handleInitialData());
+  }
+
+  render() {
+    refreshUser(this.props);
+    const { loading } = this.props;
+
+    return loading ? (
+      <div>
+        <ReactLoading
+          type={'bars'}
+          color={'#3cc6bc'}
+          height={'10%'}
+          width={'10%'}
+          className="loading-container"
+        />
+      </div>
+    ) : (
+      <Routing />
+    );
+  }
 }
 
-export default App;
+const mapStateToProps = ({ loading, authedUser }) => {
+  return {
+    loading,
+    isAuth: Object.keys(authedUser).length !== 0,
+  };
+};
+
+export default connect(mapStateToProps)(App);
+
+const refreshUser = (props) => {
+  const user = decodeToken();
+
+  if (user) {
+    const currentTime = Date.now() / 1000;
+    if (user.exp < currentTime) {
+      localStorage.removeItem(tokenKey);
+      props.history.push('/');
+    }
+    props.dispatch(setAuthedUser(user));
+    return user;
+  }
+};
